f3f0ad8fa0e07af94733810b27f0d547a0a2d529
Matheus Lima <matheus_sousa@protonmail.com>
2020-07-12 19:39:08 -0300

    uri

---
+++ TrocaEmVetor.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int N[20];
+    int aux;
+
+    for(int i = 0; i < 20; i++){
+        cin >> N[i];
+    }
+
+    for(int i = 0; i <= 9; i++){
+        aux = N[i];
+        N[i] = N[19-i];
+        N[19-i] = aux;
+    }
+
+    for(int i = 0; i <= 19; i++){
+        cout << "N[" << i << "] = " << N[i] << endl; 
+    }
+
+    return 0;
+    
+}
