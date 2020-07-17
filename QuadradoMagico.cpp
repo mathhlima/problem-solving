80188c3e313101d4955f748958b3d66f1572e202
Matheus Lima <matheus_sousa@protonmail.com>
2020-07-17 00:13:58 -0300

    neps

--- QuadradoMagico.cpp
+++ QuadradoMagico.cpp
@@ -6,11 +6,11 @@ int main(){
     cin >> n;
 
     int quad[n][n];
-    int somaArray[10];
-    int soma;
-    int somaLinha = 0;
-    int SomaColuna = 0;
-    int somaDiagonal = 0;
+    int somaLinha[n];
+    int somaColuna[n];
+    int somaDiag[n];
+    int soma = 0;
+
 
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
@@ -23,11 +23,17 @@ int main(){
             soma += quad[i][j];
             //cout << soma[i] << endl;
         }
-        somaArray[i] = soma;
+        somaLinha[i] = soma;
         soma = 0;
     }
-    for(int i = 0; i < 10; i++){
-        cout << somaArray[i] << endl;
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            soma += quad[j][i];
+        }
+
+        somaColuna[i] = soma;
+        soma = 0;
     }
     /*for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
