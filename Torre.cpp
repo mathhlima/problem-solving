4a113688cd5a6400935435ea684e9ce66c91be13
Matheus Lima <matheus_sousa@protonmail.com>
2020-07-21 22:05:03 -0300

    neps

--- Torre.cpp
+++ Torre.cpp
@@ -3,7 +3,26 @@ using namespace std;
 
 int main() {
 
-    
+    int n;
+    int somaColuna[n];
+    int soma;
+    int somaLinha[n];
+
+    cin >> n;
+
+    int xad[n][n];
+
+    for(int i = 0; i< n; i ++){
+        for(int j = 0; j < n; j++){
+            cin >> xad[i][j];
+            soma += xad[i][j];
+        }
+        somaLinha[i] = soma;
+    }
+
+    for(int i = 0; i < n; i++){
+        cout << somaLinha << endl;
+    }
     
     
     return 0;
